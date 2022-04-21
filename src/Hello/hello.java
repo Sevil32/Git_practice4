3a86c0a14aa7bce129b9d205d46381336da5269d
sevil32 <aydogansevilfr@gmail.com>
2022-04-21 11:06:13 +0200

    Testcase003 started

--- src/Hello/hello.java
+++ src/Hello/hello.java
@@ -4,7 +4,8 @@ public class hello {
     public static void main(String[] args) {
         System.err.println("hello world");
 
-        System.out.println("I am learning github");;
+        System.out.println("I am learning github");
+        ;
 
     }
 }
