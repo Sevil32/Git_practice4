4a1ef5fa2d1bd4a18d688ea5936aa0dfc14c006c
sevil32 <aydogansevilfr@gmail.com>
2022-04-21 10:57:30 +0200

    step 2

--- src/Hello/hello.java
+++ src/Hello/hello.java
@@ -3,7 +3,7 @@ package Hello;
 public class hello {
     public static void main(String[] args) {
         System.err.println("hello world");
-
+        System.out.println("I am learning github");;
 
     }
 }
